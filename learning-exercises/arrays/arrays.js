3ea885e9f968ded54f69cf203d615ddcb5d183d4
Mehar Lall <mehar.lall@gmail.com>
2026-09-26 00:50:47 +1200

    Add shopping basket project

--- learning-exercises/arrays/arrays.js
+++ learning-exercises/arrays/arrays.js
@@ -1,5 +1,25 @@
-fruits = ["apple", "banana", "cherry", "date", "elderberry"];
+const fruits = ["apple", "banana", "orange"];
 
-for (fruit in fruits) {
-    console.log(fruits[fruit]);
+// // Add a new fruit to the end of the array
+// fruits.push("coconut");
+
+// // Remove the last fruit from the array
+// fruits.pop(); 
+
+// // Add a new fruit to the beginning of the array
+// fruits.unshift("mango");
+
+// // Remove the first fruit from the array
+// fruits.shift();
+
+console.log(fruits.length);
+
+console.log(fruits.includes("banana"));
+
+fruits.push("watermelon");
+
+fruits.shift();
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(`Fruit ${i + 1}: ${fruits[i]}`);
 }
